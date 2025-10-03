7d53e99f293b231584e51d2aca00a4245b1653a6
brayan basallo soto <basallo17@gmail.com>
2025-10-03 08:34:41 -0500

    refactor(uibutton): improve component

--- src/components/ui/UiButton.vue
+++ src/components/ui/UiButton.vue
@@ -4,7 +4,7 @@ import type { Component } from 'vue'
 type buttonVariant = 'primary' | 'secondary' | 'tertiary'
 
 interface Props {
-  icon: Component
+  icon?: Component
   disabled?: boolean
   rounded?: boolean
   variant?: buttonVariant
@@ -22,10 +22,14 @@ withDefaults(defineProps<Props>(), {
       rounded ? 'px-2.5 w-11 h-11' : 'px-5 py-2.5',
       {
         'bg-primary hover:bg-secondary text-white': variant === 'primary',
-        'bg-white hover:bg-secondary-dark text-golden disabled:text-gray-200': variant === 'secondary',
-        'bg-white hover:bg-secondary-dark text-gray-300 disabled:text-gray-200': variant === 'tertiary'
-      }
-    ]" :disabled="disabled">
+        'bg-white hover:bg-gray-50 text-golden disabled:text-gray-200':
+          variant === 'secondary',
+        'bg-white hover:bg-gray-50 text-gray-300 disabled:text-gray-200':
+          variant === 'tertiary',
+      },
+    ]"
+    :disabled="disabled"
+  >
     <component :is="icon" v-if="icon" :class="rounded ? 'w-7 h-7' : 'w-4 h-4'" />
     <slot></slot>
   </button>
